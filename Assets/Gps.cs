9f8037f554fb00c5e3ff803c1c67cc0db2b0afc3
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-03-31 17:59:42 +0530

    Data getting from server

--- Assets/Gps.cs
+++ Assets/Gps.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gps : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    //public Text myLatText;
+    //public Text myLonText;
+    public static float myLattitude;
+    public static float myLongitude;
+    public static Gps instance;
+    public void Awake()
     {
-        
+        if(instance==null)
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        if(Input.location.isEnabledByUser)
+        {
+            myLattitude= Input.location.lastData.latitude ;
+            myLongitude= Input.location.lastData.longitude ;
+           
+        }
+        //Debug.Log(myLattitude);
+        //Debug.Log(myLongitude);
+       // myLatText.text = myLattitude.ToString();
+        //myLonText.text = myLongitude.ToString();
+
+    }
+    IEnumerator Start()
+    {
+        // Check if the user has location service enabled.
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        // Starts the location service.
+        Input.location.Start(5f,20f);
+
+        // Waits until the location service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // If the service didn't initialize in 20 seconds this cancels location service use.
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        // If the connection failed this cancels location service use.
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // If the connection succeeded, this retrieves the device's current location and displays it in the Console window.
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stops the location service if there is no need to query location updates continuously.
+        //Input.location.Stop();
     }
 }
---
+++ Assets/Scenes/SceneController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static string keyWord;
+    void Start()
+    {
+        //StartCoroutine(GetRequest("https://bit.ly/3iSheyg"));
+        StartCoroutine(GetRequest());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Quit()
+    {
+        Application.Quit();
+
+           
+    }
+    public void GoNextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void KeyWordForGoogle(string keyWordName)
+    {
+        keyWord = keyWordName;
+        Debug.Log(keyWord);
+        //string url = "https://bit.ly/3iSheyg";
+        // StartCoroutine(GetRequest(url));
+        //StartCoroutine(GetRequest());
+    }
+    IEnumerator GetRequest()
+    {
+        string uri = "https://bit.ly/3iSheyg";
+        using (UnityEngine.Networking.UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            Debug.Log("In Coroutine Function");
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
+}
